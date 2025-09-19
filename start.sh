e7ae610d40c0f33564b85fb71a2cce65705306cf
Daniele De Rossi <daniele.dr00@gmail.com>
2025-09-19 18:18:20 +0200

    removed linux/amd64 from build_gradle command

--- start.sh
+++ start.sh
@@ -4,25 +4,24 @@
 cd "./gatewayAPI"
 
 # Build the gatewayAPI image
-sudo ./gradlew bootBuildImage --imageName=g19/gateway_api --platform linux/amd64
-
+sudo ./gradlew bootBuildImage --imageName=g19/gateway_api
 # Navigate to CRM directory
 cd "../CRM"
 
 # Build the CRM image
-sudo ./gradlew bootBuildImage --imageName=g19/crm --platform linux/amd64
+sudo ./gradlew bootBuildImage --imageName=g19/crm
 
 # Navigate to communication_manager directory
 cd "../communication_manager"
 
 # Build the communication_manager image
-sudo ./gradlew bootBuildImage --imageName=g19/communication_manager --platform linux/amd64
+sudo ./gradlew bootBuildImage --imageName=g19/communication_manager
 
 # Navigate to user-interface directory
 cd "../user-interface/JobPlacementServices"
 
 # Build the user-interface image
-sudo docker build -t g19/user-interface . --platform linux/amd64
+sudo docker build -t g19/user-interface .
 cd ".."
 cd ".."
 
