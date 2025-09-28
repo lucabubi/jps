08e613319de18f86b24ade666433f6b194a5312c
Luca Barbato <luca.barbato@studenti.polito.it>
2025-09-28 21:41:57 +0200

    feat: added apple-like "hello" effect

--- user-interface/JobPlacementServices/src/components/dashboard/HomeWelcome.tsx
+++ user-interface/JobPlacementServices/src/components/dashboard/HomeWelcome.tsx
@@ -1,5 +1,7 @@
+import { AppleHelloEnglishEffect } from "@/components/ui/shadcn-io/apple-hello-effect";
+
 export default function HomeWelcome() {
     return (
-        <></>
+        <AppleHelloEnglishEffect speed={1.1} />
     )
 }
